7ee4b492c15b9322f58e71c31dc73c745832b308
Clarke <sclarke5555@gmail.com>
2024-02-07 12:59:56 -0500

    first commit, setup chrome extension

---
+++ public/content.js
@@ -0,0 +1,6 @@
+const continueWatching = document.querySelector('[data-list-context="continueWatching"]');
+const hero = document.querySelector(".volatile-billboard-animations-container");
+
+if(hero) {
+  hero.appendChild(continueWatching);
+}
